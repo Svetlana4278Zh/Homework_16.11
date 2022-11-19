f257744ec91a413679bb3707d468445a6fe1fdc6
Svetlana4278Zh <svetlana.zh@inbox.ru>
2022-11-19 18:31:08 +0300

    Beginning

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задача 1, 2
+        System.out.println("Задача 1, 2");
+        int clientOS = 0; //0 — iOS, 1 — Android
+        int clientDeviceYear = 2015;
+        if (clientOS==0&&clientDeviceYear<2015){
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS==0){
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS==1&&clientDeviceYear<2015){
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS==1){
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        //Задача 3
+        System.out.println("\nЗадача 3");
+        int year = 2003;
+        if (year%4!=0){
+            System.out.println(year+" год не является високосным");
+        } else if (year%400==0){
+            System.out.println(year+" год является високосным");
+        } else if (year%100==0){
+            System.out.println(year+" год не является високосным");
+        } else {
+            System.out.println(year+" год является високосным");
+        }
     }
 }
